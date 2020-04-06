f873c93663dba02f535300897bd2b31df531af15
Harald Reingruber <harald.reingruber@gmx.at>
2020-04-06 16:48:03 +0200

    Fix test issue

--- client-app/infomont/lib/result_page.dart
+++ client-app/infomont/lib/result_page.dart
@@ -24,7 +24,7 @@ class _ResultPageState extends State<ResultPage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text('Results:'),
+        title: Text(widget.title),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
--- client-app/infomont/test/result_page_test.dart
+++ client-app/infomont/test/result_page_test.dart
@@ -4,13 +4,18 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:app/result_page.dart';
 
+// TODO: extract to helper file
+Widget buildTestableWidget(Widget widget) {
+  return MediaQuery(data: MediaQueryData(), child: MaterialApp(home: widget));
+}
+
 void main() {
   testWidgets('Displays 3 different route options', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(ResultPage());
+    await tester.pumpWidget(buildTestableWidget(ResultPage( title: 'Results:')));
 
-    expect(find.text('Route 1'), findsOneWidget);
-    expect(find.text('Route 2'), findsOneWidget);
-    expect(find.text('Route 3'), findsOneWidget);
+    expect(find.text('Route 1: Test'), findsOneWidget);
+    expect(find.text('Route 2: Test'), findsOneWidget);
+    expect(find.text('Route 3: Test'), findsOneWidget);
   });
 }
