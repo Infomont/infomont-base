7268b9bd730dd7c1c003d419af8a7443775e7244
Harald Reingruber <harald.reingruber@gmx.at>
2020-04-08 11:46:33 +0200

    Refactor: Extract variables for hard-coded UI strings

--- client-app/infomont/lib/hike_option_widget.dart
+++ client-app/infomont/lib/hike_option_widget.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
 class HikeOptionWidget extends StatelessWidget {
+  final optionName = 'Breaza - Peaks Brezei - Chalet Urlea';
+  final duration = '3h 30min';
+  final shortDescription = 'From Breaza, from the final station of the bus, the marks with blue triangle lead us to the point called "Pe Livadia" ("At the orchard"), where there is a orchard nursery. Here there is a pole with indicating plates.';
+  final marks = 'Red Triangle';
+  final marksQuality = '★★★★☆';
+  final difficulty = '★☆☆☆☆';
+
   const HikeOptionWidget({
     Key key,
   }) : super(key: key);
@@ -9,26 +16,23 @@ class HikeOptionWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(child: Column(children: <Widget>[
       ListTile(
-        title: Text('Breaza - Peaks Brezei - Chalet Urlea'),
-        subtitle: Text('Duration: ' '3h 30min'),
+        title: Text(optionName),
+        subtitle: Text('Duration: $duration'),
       ),
       Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
-            Container(child: Text('Description: ' 'From Breaza, from the final station of the bus, the marks with blue triangle lead us to the point called "Pe Livadia" ("At the orchard"), where there is a orchard nursery. Here there is a pole with indicating plates.')),
+            Container(child: Text('Description: $shortDescription')),
             Text(
-              'Mark: ' 'Red Triangle',
+              'Marks: $marks',
               style: const TextStyle(
                 fontSize: 12.0,
                 color: Colors.black87,
               ),
             ),
             Text(
-            'Mark quality: ' 'good'
-                ' · '
-                'Difficulty: ' 'easy'
-                ' ★',
+            'Marks quality: $marksQuality · Difficulty: $difficulty',
               style: const TextStyle(
                 fontSize: 12.0,
                 color: Colors.black54,
