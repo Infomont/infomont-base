b107b29f33bdb8ffa1dd2d0be310c1e686509224
Harald Reingruber <harald.reingruber@gmx.at>
2020-04-10 17:22:00 +0200

    Fix: json String type was deduced as Future<String>

--- client-app/infomont/test/json_adapter_stub.dart
+++ client-app/infomont/test/json_adapter_stub.dart
@@ -3,14 +3,15 @@ import 'package:app/json_adapter.dart';
 class JsonAdapterStub extends JsonAdapter{
   JsonAdapterStub();
 
-  var _json;
+  String _json;
 
   set json(value) {
     _json = value;
   }
 
   @override
-  fetchJson(){
+  Future<String> fetchJson() async {
+
     return _json;
   }
 
