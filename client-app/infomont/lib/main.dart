8759a6ea0ce88205afe993443ca749492d5bd412
Harald Reingruber <harald.reingruber@gmx.at>
2020-05-28 10:36:17 +0200

    Update colors and start screen title
    Co-Authored-By: Adrian Bolboaca <adi_bolb123@yahoo.com>

--- client-app/infomont/lib/main.dart
+++ client-app/infomont/lib/main.dart
@@ -4,6 +4,19 @@ import 'start_page.dart';
 
 void main() => runApp(InfoMontApp());
 
+const Map<int, Color> color = {
+  50: Color.fromRGBO(136, 14, 79, .1),
+  100: Color.fromRGBO(136, 14, 79, .2),
+  200: Color.fromRGBO(136, 14, 79, .3),
+  300: Color.fromRGBO(136, 14, 79, .4),
+  400: Color.fromRGBO(136, 14, 79, .5),
+  500: Color.fromRGBO(136, 14, 79, .6),
+  600: Color.fromRGBO(136, 14, 79, .7),
+  700: Color.fromRGBO(136, 14, 79, .8),
+  800: Color.fromRGBO(136, 14, 79, .9),
+  900: Color.fromRGBO(136, 14, 79, 1),
+};
+
 class InfoMontApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -20,8 +33,40 @@ class InfoMontApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+
+          textTheme: TextTheme(
+            body1: TextStyle(color: Colors.black),
+            body2: TextStyle(color: Colors.black),
+            button: TextStyle(color: Colors.black),
+            caption: TextStyle(color: Colors.indigo),
+            display1: TextStyle(color: Colors.black),
+            display2: TextStyle(color: Colors.black),
+            display3: TextStyle(color: Colors.black),
+            display4: TextStyle(color: Colors.black),
+            headline: TextStyle(color: Colors.amberAccent),
+            subhead: TextStyle(color: Colors.red),
+            title: TextStyle(color: Color(0xFFE0E2DB)),
+          ),
+          inputDecorationTheme: InputDecorationTheme(
+            fillColor: Color(0xFFFDD124),
+            filled: true,
+          ),
+
+          cursorColor: Color(0xFF5D576B),
+          primarySwatch: MaterialColor(0xFF445B18, color),
+          scaffoldBackgroundColor: Color(0xFF5D5726),
+          accentColor: Color(0xFFE0E2DB),
+          buttonTheme: ButtonThemeData(textTheme: ButtonTextTheme.accent),
+          fontFamily: "Roboto",
+          canvasColor: Color(0xFF445B18),
+          hintColor: Color(0xFF8884FF),
+
+
+
+
+
+
+      ) ,
       //home: StartPage(title: 'Start Hiking'),
       showSemanticsDebugger: false, // can be used to debug layout or UI issues
       // Start the app with the "/" named route. In this case, the app starts
@@ -29,7 +74,7 @@ class InfoMontApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         // When navigating to the "/" route, build the StartPage widget.
-        '/': (context) => StartPage(title: 'Start Hiking')
+        '/': (context) => StartPage(title: 'Start Hiking with Infomont')
       },
     );
   }
