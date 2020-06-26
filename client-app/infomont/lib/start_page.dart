40c9e2d73f68f97314b53401415597c470c60952
Harald Reingruber <harald.reingruber@gmx.at>
2020-06-26 12:09:04 +0200

    Extract point_typeahead_builder.dart
    Co-Authored-By: Adrian Bolboaca <adi_bolb123@yahoo.com>

--- client-app/infomont/lib/start_page.dart
+++ client-app/infomont/lib/start_page.dart
@@ -1,10 +1,8 @@
-import 'package:app/db_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_typeahead/flutter_typeahead.dart';
 
 import 'hike_option_db_provider.dart';
 import 'hike_option_search_parameters.dart';
-import 'point.dart';
+import 'point_typeahead_builder.dart';
 import 'result_page.dart';
 
 class StartPage extends StatefulWidget {
@@ -141,38 +139,5 @@ class _StartPageState extends State<StartPage> {
         ),
       ),
     );
-  }
-
-  TypeAheadFormField<Point> buildPointTypeAheadField(
-      String label, TextEditingController typeAheadController, onSelect(int id, String name)) {
-    return TypeAheadFormField<Point>(
-        textFieldConfiguration: TextFieldConfiguration(
-            controller: typeAheadController,
-            style: TextStyle(color: Color(0xFF5D576B)),
-            decoration: InputDecoration(
-              labelText: label,
-              labelStyle: TextStyle(color: Colors.grey),
-            )),
-        suggestionsCallback: (pattern) {
-          return DBProvider.db.searchPointByName(pattern);
-        },
-        itemBuilder: (context, suggestion) {
-          return ListTile(
-            title: Text(suggestion.name),
-          );
-        },
-        transitionBuilder: (context, suggestionsBox, controller) {
-          return suggestionsBox;
-        },
-        onSuggestionSelected: (suggestion) {
-          typeAheadController.text = suggestion.name;
-          onSelect(suggestion.id, suggestion.name);
-        },
-        validator: (value) {
-          if (value.isEmpty) {
-            return 'Please enter some text';
-          }
-          return null;
-        });
   }
 }
